162c6a8f3a3c46514b70e17d80b58523005f3865
Dravik Italiya <20bce509@nirmauni.ac.in>
2022-06-14 23:27:33 +0530

    add date in checklog

--- home/views.py
+++ home/views.py
@@ -131,7 +131,7 @@ def Start_Webcam(request):
 
 class VideoCamera1(object):
     def __init__(self):
-        self.video = cv2.VideoCapture('http://11.0.0.26:8888/out.jpg')
+        self.video = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()
